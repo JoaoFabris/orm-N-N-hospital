d8a71993b4bb7db729c462b777dbe532ea242484
Joao Fabris <jvfabris21@gmail.com>
2025-03-13 18:04:05 -0300

    feat: add plan model

--- src/models/Plan.js
+++ src/models/Plan.js
@@ -2,20 +2,22 @@ module.exports = (sequelize, DataTypes) => {
     const Plan = sequelize.define(
       "Plan",
       {
-        planId: {
+        plan_id: {
           type: DataTypes.INTEGER,
           primaryKey: true,
+          autoIncrement: true,
         },
         coverage: {
           type: DataTypes.STRING,
           allowNull: false,
         },
         price: {
-          type: DataTypes.DOUBLE,
+          type: DataTypes.DOUBLE, // useful for scientific calculations
           allowNull: false,
         },
       },
       {
+        underscored: true,
         tableName: "Plans",
         timestamps: false, // Se não quiser createdAt e updatedAt
       }
@@ -28,4 +30,6 @@ module.exports = (sequelize, DataTypes) => {
         as: "patients",
       });
     };
+
+    return Plan;
 }
