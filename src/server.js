dfc384297410092afc3f665f6e1533208988ff0e
Joao Fabris <jvfabris21@gmail.com>
2025-03-21 21:58:37 -0300

    feat: config and server update and prettierrc

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const app = require('./app');
+const { sequelize } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Port: ${PORT}`);
 });
+
+sequelize
+  .sync({ force: true }) // Usa force: true para recriar as tabelas
+  .then(() => {
+    console.log('Tabelas criadas ou recriadas com sucesso!');
+  })
+  .catch((err) => {
+    console.error('Erro ao criar as tabelas:', err);
+  });
